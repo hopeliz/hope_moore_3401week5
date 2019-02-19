c9b821fe6bf3300de5a9af53b44d67707a1a67b3
Hope Moore <hopemoore@gmail.com>
2019-02-18 23:43:27 -0500

    Adding project

---
+++ 3501_Week5_Moore/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.z < 21.1)
+        {
+            transform.position += Vector3.forward / 10;
+        }
+
+        //Ray playerRay = new Ray(transform.position, Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, 5.0F))
+        {
+            print(hit.collider.name);
+        }
+        else
+        {
+            print("NOTHING AHEAD");
+        }
+	}
+}
